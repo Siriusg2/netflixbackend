f006025bf73930deedf1d10bda1a7ea3ec911eaa
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-05-08 22:34:40 -0500

    add string to test if server is working

--- src/main/java/com/consulti/templatespringboot/TemplateSpringbootApplication.java
+++ src/main/java/com/consulti/templatespringboot/TemplateSpringbootApplication.java
@@ -1,13 +1,18 @@
 package com.consulti.templatespringboot;
 
+import javax.sound.sampled.Port;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class TemplateSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateSpringbootApplication.class, args);
+		System.out.println("server listening on port 5000" );
+
 	}
 
 }

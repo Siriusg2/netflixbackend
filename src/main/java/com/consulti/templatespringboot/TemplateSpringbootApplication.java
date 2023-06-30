1b862d71d03d301376f24542ada01e7ca6c9cde9
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-06-30 18:24:43 -0500

    final attempt to fix .env file

--- src/main/java/com/consulti/templatespringboot/TemplateSpringbootApplication.java
+++ src/main/java/com/consulti/templatespringboot/TemplateSpringbootApplication.java
@@ -1,16 +1,13 @@
 package com.consulti.templatespringboot;
 
-import com.consulti.templatespringboot.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Import(CorsConfig.class)
 public class TemplateSpringbootApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TemplateSpringbootApplication.class, args);
-    System.out.println("server listening on port 5000");
+    System.out.println("Server listening on port 5000");
   }
 }

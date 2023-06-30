6ea34d6ef50a09ad31cd76da8f996c20e4df74cb
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-06-30 14:55:54 -0500

    adding enviroment variables to deploy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:11
 
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} netflixconsulti.jar
 EXPOSE 5000
+ADD ./target/netflixconsulti.jar app.jar
 
 ENTRYPOINT ["java", "-jar", "app.jar", "spring.config.location=file:application2.properties"]
--- src/main/java/com/consulti/templatespringboot/TemplateSpringbootApplication.java
+++ src/main/java/com/consulti/templatespringboot/TemplateSpringbootApplication.java
@@ -1,26 +1,16 @@
 package com.consulti.templatespringboot;
 
-
-
-
+import com.consulti.templatespringboot.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.Import;
 
-import com.consulti.templatespringboot.config.CorsConfig;
-
-
-
 @SpringBootApplication
 @Import(CorsConfig.class)
-
 public class TemplateSpringbootApplication {
-	
-	public static void main(String[] args) {
-		
-		SpringApplication.run(TemplateSpringbootApplication.class, args);
-		System.out.println("server listening on port 5000" );
-		
-	}
 
+  public static void main(String[] args) {
+    SpringApplication.run(TemplateSpringbootApplication.class, args);
+    System.out.println("server listening on port 5000");
+  }
 }
